ba08ca8a5054a35d72de69041ee1cd34a9335208
Estevao Vaz <estevaobarreto8@gmail.com>
2019-06-13 17:47:18 -0300

    QUESTAO 02 - META - PROCESSO SELETIVO

--- META-QUESTAO2/Program.cs
+++ META-QUESTAO2/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace META_QUESTAO2
 {
@@ -10,6 +7,38 @@ namespace META_QUESTAO2
     {
         static void Main(string[] args)
         {
+            #region META QUESTAO 2
+            string str1, str2, str3, str4, str5;
+            string result;
+            Console.WriteLine("Exemplo de entrada -> { [ () ] }");
+            Console.WriteLine("Entre com 5 caracteres");
+            Console.WriteLine("Sendo que a cada espaço conta um ou seja -> Ex () é igual a 1, e ( ) é igual a 2");
+         
+            string[] linha = Console.ReadLine().Split(' ');
+            str1 = linha[0];
+            str2 = linha[1];
+            str3 = linha[2];
+            str4 = linha[3];
+            str5 = linha[4];
+
+            result = string.Join(" ", str1 + str2 + str3 + str4 + str5);
+
+
+            Console.WriteLine("STRING É BALANCEADA?");
+
+            if(str1 == "{" && str5 == "}" && str2 == "[" && str4 == "]" && str3 == "()")
+            {
+                Console.WriteLine("SIM");
+            }
+
+            else
+            {
+                Console.WriteLine("NÃO");
+            }
+
+            Console.WriteLine(result);
+
+            #endregion
         }
     }
 }
